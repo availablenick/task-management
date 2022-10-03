bf8a062838e1b650bb14ac382f24cf7aca0bf4b0
availablenick <leoap711@gmail.com>
2022-10-03 20:50:08 -0300

    refactor(user tests): change hardcoded routes to named routes

--- tests/Feature/UserManagementTest.php
+++ tests/Feature/UserManagementTest.php
@@ -14,7 +14,7 @@ class UserManagementTest extends TestCase
 
     public function test_user_page_can_be_rendered()
     {
-        $response = $this->get('/users');
+        $response = $this->get(route('users.index'));
 
         $response->assertStatus(200);
     }
@@ -22,42 +22,44 @@ class UserManagementTest extends TestCase
     public function test_user_details_page_can_be_rendered()
     {
         $user = User::factory()->create();
-        $response = $this->get('/users/' . $user->id);
+        $response = $this->get(route('users.show', $user));
 
         $response->assertStatus(200);
     }
 
     public function test_user_cannot_be_created_without_name()
     {
-        $response = $this->loginAsAdmin()->post('/users');
+        $response = $this->loginAsAdmin()->post(route('users.store'));
 
         $response->assertInvalid(['name']);
     }
 
     public function test_user_cannot_be_created_without_email()
     {
-        $response = $this->loginAsAdmin()->post('/users');
+        $response = $this->loginAsAdmin()->post(route('users.store'));
 
         $response->assertInvalid(['email']);
     }
 
     public function test_user_cannot_be_created_with_invalid_email()
     {
-        $response = $this->loginAsAdmin()->post('/users', ['email' => 'test']);
+        $response = $this->loginAsAdmin()->post(route('users.store'), [
+            'email' => 'test'
+        ]);
 
         $response->assertInvalid(['email']);
     }
 
     public function test_user_cannot_be_created_without_password()
     {
-        $response = $this->loginAsAdmin()->post('/users');
+        $response = $this->loginAsAdmin()->post(route('users.store'));
 
         $response->assertInvalid(['password']);
     }
 
     public function test_user_cannot_be_created_with_invalid_password_length()
     {
-        $response = $this->loginAsAdmin()->post('/users', [
+        $response = $this->loginAsAdmin()->post(route('users.store'), [
             'password' => '1234567',
         ]);
 
@@ -66,7 +68,7 @@ class UserManagementTest extends TestCase
 
     public function test_user_cannot_be_created_without_password_confirmation()
     {
-        $response = $this->loginAsAdmin()->post('/users', [
+        $response = $this->loginAsAdmin()->post(route('users.store'), [
             'password' => 'test_password',
         ]);
 
@@ -75,7 +77,7 @@ class UserManagementTest extends TestCase
 
     public function test_user_cannot_be_created_with_wrong_password_confirmation()
     {
-        $response = $this->loginAsAdmin()->post('/users', [
+        $response = $this->loginAsAdmin()->post(route('users.store'), [
             'password' => 'test_password',
             'password_confirmation' => 'test',
         ]);
@@ -86,7 +88,7 @@ class UserManagementTest extends TestCase
     public function test_user_cannot_be_updated_with_invalid_email()
     {
         $user = User::factory()->create();
-        $response = $this->loginAsAdmin()->put('/users/' . $user->id, [
+        $response = $this->loginAsAdmin()->put(route('users.update', $user), [
             'email' => 'test_email',
         ]);
 
@@ -95,22 +97,22 @@ class UserManagementTest extends TestCase
 
     public function test_guest_cannot_access_creation_page()
     {
-        $response = $this->get('/users/create');
+        $response = $this->get(route('users.create'));
 
-        $response->assertRedirect('/login');
+        $response->assertRedirect(route('login'));
     }
 
     public function test_guest_cannot_access_edit_page()
     {
         $user = User::factory()->create();
-        $response = $this->get('/users/' . $user->id . '/edit');
+        $response = $this->get(route('users.edit', $user));
 
-        $response->assertRedirect('/login');
+        $response->assertRedirect(route('login'));
     }
 
     public function test_guest_cannot_create_users()
     {
-        $response = $this->post('/users', [
+        $response = $this->post(route('users.store'), [
             'name' => 'test_name',
             'email' => 'test@test.com',
             'password' => 'test_password',
@@ -121,13 +123,13 @@ class UserManagementTest extends TestCase
             'email' => 'test@test.com',
         ]);
 
-        $response->assertRedirect('/login');
+        $response->assertRedirect(route('login'));
     }
 
     public function test_guest_cannot_edit_users()
     {
         $user = User::factory()->create();
-        $response = $this->put('/users/' . $user->id, [
+        $response = $this->put(route('users.update', $user), [
             'name' => 'edit_' . $user->name,
             'email' => 'edit_' . $user->email,
             'password' => 'edit_password',
@@ -138,40 +140,40 @@ class UserManagementTest extends TestCase
             'email' => 'edit_' . $user->email,
         ]);
 
-        $response->assertRedirect('/login');
+        $response->assertRedirect(route('login'));
     }
 
     public function test_guest_cannot_delete_users()
     {
         $user = User::factory()->create();
-        $response = $this->delete('/users/' . $user->id);
+        $response = $this->delete(route('users.destroy', $user));
 
         $this->assertModelExists($user);
         $this->assertNotSoftDeleted($user);
-        $response->assertRedirect('/login');
+        $response->assertRedirect(route('login'));
     }
 
     public function test_non_admin_user_cannot_access_creation_page()
     {
         $user = User::factory()->create();
-        $response = $this->actingAs($user)->get('/users/create');
+        $response = $this->actingAs($user)->get(route('users.create'));
 
-        $response->assertRedirect('/unauthorized');
+        $response->assertRedirect(route('unauthorized'));
     }
 
     public function test_non_admin_user_cannot_access_another_user_edit_page()
     {
         $user1 = User::factory()->create();
         $user2 = User::factory()->create();
-        $response = $this->actingAs($user2)->get('/users/' . $user1->id . '/edit');
+        $response = $this->actingAs($user2)->get(route('users.edit', $user1));
 
-        $response->assertRedirect('/unauthorized');
+        $response->assertRedirect(route('unauthorized'));
     }
 
     public function test_non_admin_user_can_access_their_edit_page()
     {
         $user = User::factory()->create();
-        $response = $this->actingAs($user)->get('/users/' . $user->id . '/edit');
+        $response = $this->actingAs($user)->get(route('users.edit', $user));
 
         $response->assertStatus(200);
     }
@@ -179,7 +181,7 @@ class UserManagementTest extends TestCase
     public function test_non_admin_user_cannot_create_users()
     {
         $user = User::factory()->create();
-        $response = $this->actingAs($user)->post('/users', [
+        $response = $this->actingAs($user)->post(route('users.store'), [
             'name' => 'test_name',
             'email' => 'test@test.com',
             'password' => 'test_password',
@@ -190,14 +192,14 @@ class UserManagementTest extends TestCase
             'email' => 'test@test.com',
         ]);
 
-        $response->assertRedirect('/unauthorized');
+        $response->assertRedirect(route('unauthorized'));
     }
 
     public function test_non_admin_user_cannot_edit_another_user()
     {
         $user1 = User::factory()->create();
         $user2 = User::factory()->create();
-        $response = $this->actingAs($user2)->put('/users/' . $user1->id, [
+        $response = $this->actingAs($user2)->put(route('users.update', $user1), [
             'name' => 'edit_' . $user1->name,
             'email' => 'edit_' . $user1->email,
             'password' => 'edit_password',
@@ -208,7 +210,7 @@ class UserManagementTest extends TestCase
             'email' => 'edit_' . $user1->email,
         ]);
 
-        $response->assertRedirect('/unauthorized');
+        $response->assertRedirect(route('unauthorized'));
     }
 
     public function test_non_admin_user_can_edit_themself()
@@ -219,36 +221,36 @@ class UserManagementTest extends TestCase
             'name' => 'edit_' . $user->name,
         ];
 
-        $response = $this->actingAs($user)->put('/users/' . $user->id, $newData);
+        $response = $this->actingAs($user)->put(route('users.update', $user), $newData);
 
         $this->assertDatabaseHas('users', $newData);
-        $response->assertRedirect('/users/' . $user->id);
+        $response->assertRedirect(route('users.show', $user));
     }
 
     public function test_non_admin_user_cannot_delete_another_user()
     {
         $user1 = User::factory()->create();
         $user2 = User::factory()->create();
-        $response = $this->actingAs($user2)->delete('/users/' . $user1->id);
+        $response = $this->actingAs($user2)->delete(route('users.destroy', $user1));
 
         $this->assertModelExists($user1);
         $this->assertNotSoftDeleted($user1);
-        $response->assertRedirect('/unauthorized');
+        $response->assertRedirect(route('unauthorized'));
     }
 
     public function test_non_admin_user_can_delete_themself()
     {
         $user = User::factory()->create();
-        $response = $this->actingAs($user)->delete('/users/' . $user->id);
+        $response = $this->actingAs($user)->delete(route('users.destroy', $user));
 
         $this->assertModelExists($user);
         $this->assertSoftDeleted($user);
-        $response->assertRedirect('/users');
+        $response->assertRedirect(route('users.index'));
     }
 
     public function test_admin_can_access_creation_page()
     {
-        $response = $this->loginAsAdmin()->get('/users/create');
+        $response = $this->loginAsAdmin()->get(route('users.create'));
 
         $response->assertStatus(200);
     }
@@ -256,7 +258,7 @@ class UserManagementTest extends TestCase
     public function test_admin_can_access_edit_page()
     {
         $user = User::factory()->create();
-        $response = $this->loginAsAdmin()->get('/users/' . $user->id . '/edit');
+        $response = $this->loginAsAdmin()->get(route('users.edit', $user));
 
         $response->assertStatus(200);
     }
@@ -264,7 +266,7 @@ class UserManagementTest extends TestCase
     public function test_admin_can_create_users()
     {
         Storage::fake();
-        $response = $this->loginAsAdmin()->post('/users', [
+        $response = $this->loginAsAdmin()->post(route('users.store'), [
             'name' => 'test_name',
             'email' => 'test@test.com',
             'password' => 'test_password',
@@ -280,13 +282,13 @@ class UserManagementTest extends TestCase
         $user = User::where('email', 'test@test.com')->first();
         $this->assertNotNull($user->avatar_path);
         Storage::disk()->assertExists($user->avatar_path);
-        $response->assertRedirect('/users/' . $user->id);
+        $response->assertRedirect(route('users.show', $user));
     }
 
     public function test_admin_can_edit_users()
     {
         Storage::fake();
-        $response = $this->loginAsAdmin()->post('/users', [
+        $response = $this->loginAsAdmin()->post(route('users.store'), [
             'name' => 'test_name',
             'email' => 'test@test.com',
             'password' => 'test_password',
@@ -296,7 +298,7 @@ class UserManagementTest extends TestCase
 
         $user = User::where('email', 'test@test.com')->first();
         $oldFilepath = $user->avatar_path;
-        $response = $this->loginAsAdmin()->put('/users/' . $user->id, [
+        $response = $this->loginAsAdmin()->put(route('users.update', $user), [
             'email' => 'edit_' . $user->email,
             'name' => 'edit_' . $user->name,
             'avatar' => UploadedFile::fake()->image('avatar.jpg'),
@@ -311,13 +313,13 @@ class UserManagementTest extends TestCase
         $this->assertNotNull($user->avatar_path);
         Storage::disk()->assertExists($user->avatar_path);
         Storage::disk()->assertMissing($oldFilepath);
-        $response->assertRedirect('/users/' . $user->id);
+        $response->assertRedirect(route('users.show', $user));
     }
 
     public function test_admin_can_delete_users()
     {
         Storage::fake();
-        $response = $this->loginAsAdmin()->post('/users', [
+        $response = $this->loginAsAdmin()->post(route('users.store'), [
             'name' => 'test_name',
             'email' => 'test@test.com',
             'password' => 'test_password',
@@ -326,12 +328,12 @@ class UserManagementTest extends TestCase
         ]);
 
         $user = User::where('email', 'test@test.com')->first();
-        $response = $this->loginAsAdmin()->delete('/users/' . $user->id);
+        $response = $this->loginAsAdmin()->delete(route('users.destroy', $user));
 
         $this->assertModelExists($user);
         $this->assertSoftDeleted($user);
         Storage::disk()->assertMissing($user->avatar_path);
-        $response->assertRedirect('/users');
+        $response->assertRedirect(route('users.index'));
     }
 
     /*
